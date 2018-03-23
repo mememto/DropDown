a8f1cb2abbe31c46c44f8b4efa1044a9d4c37f28
lucianotoools <luciano@toools.es>
2018-03-23 12:23:41 +0100

    updated to swift 4

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -109,10 +109,10 @@ class ViewController: UIViewController {
 		appearance.backgroundColor = UIColor(white: 1, alpha: 1)
 		appearance.selectionBackgroundColor = UIColor(red: 0.6494, green: 0.8155, blue: 1.0, alpha: 0.2)
 //		appearance.separatorColor = UIColor(white: 0.7, alpha: 0.8)
-		appearance.cornerRadius = 10
+		appearance.cornerRad = 10
 		appearance.shadowColor = UIColor(white: 0.6, alpha: 1)
-		appearance.shadowOpacity = 0.9
-		appearance.shadowRadius = 25
+		appearance.shadowOpac = 0.9
+		appearance.shadowRad = 25
 		appearance.animationduration = 0.25
 		appearance.textColor = .darkGray
 //		appearance.textFont = UIFont(name: "Georgia", size: 14)
